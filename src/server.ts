9e161922742332b4bfd645a1b0c7b385db6910c2
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-09-04 17:34:02 +0100

    pm2 live

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import {app} from './app';
 
-const port : number | string = process.env.PORT ||  9090 ;
+const port : number | string =  9090 ;
 
 app.listen(port, ()=> console.log(`Meeting Running on port ${port}`));
